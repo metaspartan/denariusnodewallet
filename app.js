ecce31a1132d88e7fd3304da4833814b676e3aa1
carsenk <carsen@btc.com>
2017-07-02 02:11:35 -0600

    Change .env link

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '.env' });
+dotenv.load({ path: '.env.example' });
 
 /**
  * Controllers (route handlers).
